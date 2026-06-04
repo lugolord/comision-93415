650dd4d2022fce146509d24a26484c117b74522f
Luis Gonzalez <lugo50635@gmail.com>
2026-06-03 21:16:40 -0300

    new: creamos el componente Sidebar y le dimos unos estilos iniciales

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,21 @@
 function Sidebar () {
   return (
-    <div className='bg-red-400 w-2/12 h-screen'>sidebar</div>
+    <aside className='py-5 px-3 flex flex-col justify-between bg-[#201f1f] w-2/12 h-screen'>
+      <header>
+        <h1 className='text-2xl text-[#d0bcff]'>Luxe Boutique</h1>
+        <p className='text-gray-400 mb-5'>Premium Tech Store</p>
+        <ul>
+          <li className='text-white'>Home</li>
+          <li className='text-white'>Categories</li>
+          <li className='text-white'>New Arrivals</li>
+          <li className='text-white'>My Orders</li>
+          <li className='text-white'>Profile</li>
+        </ul>
+      </header>
+      <button className='bg-[#d0bcff] text-purple-900 font-bold w-4/5 px-3 py-2 rounded'>
+        Shop Now
+      </button>
+    </aside>
   )
 }
 
